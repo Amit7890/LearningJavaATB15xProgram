ac032614a2e12eb66634ba0f7eb4d95999722360
Amit7890 <amitagrawal320@gmail.com>
2025-12-25 08:37:25 +0530

    change in Lab007_String

--- src/ex_02_Variable_DataType/Lab007_String.java
+++ src/ex_02_Variable_DataType/Lab007_String.java
@@ -7,8 +7,12 @@ public class Lab007_String
         String last_name = "Agrwal";
         int a = 5;
         int b = 7;
+        char c = 97;
         System.out.println(first_Name + last_name+a+b);
         System.out.println(a+b+first_Name+last_name);
         System.out.println(first_Name+last_name+(a+b));
+        System.out.println('c'+a);
+        System.out.println(a+c);
+        System.out.println(c);
     }
 }
